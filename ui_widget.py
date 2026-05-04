c0b7a8a984d6892318333296025ff437fa471d6d
EST2374 <jack.cez2004@gmail.com>
2026-05-04 19:54:48 +0200

    autostart beta

--- ui_widget.py
+++ ui_widget.py
@@ -16,40 +16,41 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QComboBox, QHBoxLayout, QLabel,
+    QListWidget, QListWidgetItem, QPushButton, QScrollArea,
     QSizePolicy, QTabWidget, QVBoxLayout, QWidget)
 
 class Ui_Widget(object):
     def setupUi(self, Widget):
         if not Widget.objectName():
             Widget.setObjectName(u"Widget")
-        Widget.resize(743, 564)
-        self.verticalLayout_2 = QVBoxLayout(Widget)
-        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        Widget.resize(890, 716)
+        self.horizontalLayout_10 = QHBoxLayout(Widget)
+        self.horizontalLayout_10.setObjectName(u"horizontalLayout_10")
         self.Menu_settings = QTabWidget(Widget)
         self.Menu_settings.setObjectName(u"Menu_settings")
         self.Menu_settings.setEnabled(True)
-        sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.Menu_settings.sizePolicy().hasHeightForWidth())
         self.Menu_settings.setSizePolicy(sizePolicy)
         self.Menu_settings.setMaximumSize(QSize(751, 571))
-        self.tab = QWidget()
-        self.tab.setObjectName(u"tab")
-        self.widget = QWidget(self.tab)
-        self.widget.setObjectName(u"widget")
-        self.widget.setGeometry(QRect(10, 30, 301, 291))
-        self.verticalLayout = QVBoxLayout(self.widget)
+        self.monitor_tab = QWidget()
+        self.monitor_tab.setObjectName(u"monitor_tab")
+        self.verticalLayout_2 = QVBoxLayout(self.monitor_tab)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout_6 = QVBoxLayout()
+        self.verticalLayout_6.setObjectName(u"verticalLayout_6")
+        self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.label = QLabel(self.widget)
+        self.label = QLabel(self.monitor_tab)
         self.label.setObjectName(u"label")
 
         self.horizontalLayout.addWidget(self.label)
 
-        self.monitors_box = QComboBox(self.widget)
+        self.monitors_box = QComboBox(self.monitor_tab)
         self.monitors_box.setObjectName(u"monitors_box")
 
         self.horizontalLayout.addWidget(self.monitors_box)
@@ -59,12 +60,12 @@ class Ui_Widget(object):
 
         self.horizontalLayout_2 = QHBoxLayout()
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.label_2 = QLabel(self.widget)
+        self.label_2 = QLabel(self.monitor_tab)
         self.label_2.setObjectName(u"label_2")
 
         self.horizontalLayout_2.addWidget(self.label_2)
 
-        self.resolution_box = QComboBox(self.widget)
+        self.resolution_box = QComboBox(self.monitor_tab)
         self.resolution_box.setObjectName(u"resolution_box")
 
         self.horizontalLayout_2.addWidget(self.resolution_box)
@@ -74,12 +75,12 @@ class Ui_Widget(object):
 
         self.horizontalLayout_3 = QHBoxLayout()
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.label_3 = QLabel(self.widget)
+        self.label_3 = QLabel(self.monitor_tab)
         self.label_3.setObjectName(u"label_3")
 
         self.horizontalLayout_3.addWidget(self.label_3)
 
-        self.position_box = QComboBox(self.widget)
+        self.position_box = QComboBox(self.monitor_tab)
         self.position_box.setObjectName(u"position_box")
 
         self.horizontalLayout_3.addWidget(self.position_box)
@@ -89,12 +90,12 @@ class Ui_Widget(object):
 
         self.horizontalLayout_4 = QHBoxLayout()
         self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
-        self.label_4 = QLabel(self.widget)
+        self.label_4 = QLabel(self.monitor_tab)
         self.label_4.setObjectName(u"label_4")
 
         self.horizontalLayout_4.addWidget(self.label_4)
 
-        self.scale_box = QComboBox(self.widget)
+        self.scale_box = QComboBox(self.monitor_tab)
         self.scale_box.setObjectName(u"scale_box")
 
         self.horizontalLayout_4.addWidget(self.scale_box)
@@ -102,32 +103,105 @@ class Ui_Widget(object):
 
         self.verticalLayout.addLayout(self.horizontalLayout_4)
 
-        self.Menu_settings.addTab(self.tab, "")
-        self.tab_2 = QWidget()
-        self.tab_2.setObjectName(u"tab_2")
-        self.Menu_settings.addTab(self.tab_2, "")
-        self.tab_3 = QWidget()
-        self.tab_3.setObjectName(u"tab_3")
-        self.Menu_settings.addTab(self.tab_3, "")
-        self.tab_4 = QWidget()
-        self.tab_4.setObjectName(u"tab_4")
-        self.Menu_settings.addTab(self.tab_4, "")
-        self.tab_5 = QWidget()
-        self.tab_5.setObjectName(u"tab_5")
-        self.Menu_settings.addTab(self.tab_5, "")
-        self.tab_6 = QWidget()
-        self.tab_6.setObjectName(u"tab_6")
-        self.Menu_settings.addTab(self.tab_6, "")
-        self.tab_7 = QWidget()
-        self.tab_7.setObjectName(u"tab_7")
-        self.Menu_settings.addTab(self.tab_7, "")
 
-        self.verticalLayout_2.addWidget(self.Menu_settings)
+        self.verticalLayout_6.addLayout(self.verticalLayout)
+
+        self.apply_button = QPushButton(self.monitor_tab)
+        self.apply_button.setObjectName(u"apply_button")
+
+        self.verticalLayout_6.addWidget(self.apply_button)
+
+
+        self.verticalLayout_2.addLayout(self.verticalLayout_6)
+
+        self.Menu_settings.addTab(self.monitor_tab, "")
+        self.auto_tab = QWidget()
+        self.auto_tab.setObjectName(u"auto_tab")
+        self.verticalLayout_5 = QVBoxLayout(self.auto_tab)
+        self.verticalLayout_5.setObjectName(u"verticalLayout_5")
+        self.horizontalLayout_7 = QHBoxLayout()
+        self.horizontalLayout_7.setObjectName(u"horizontalLayout_7")
+        self.verticalLayout_3 = QVBoxLayout()
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.scrollArea = QScrollArea(self.auto_tab)
+        self.scrollArea.setObjectName(u"scrollArea")
+        self.scrollArea.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollAreaWidgetContents = QWidget()
+        self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 709, 435))
+        self.verticalLayout_4 = QVBoxLayout(self.scrollAreaWidgetContents)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.current_autostart = QListWidget(self.scrollAreaWidgetContents)
+        self.current_autostart.setObjectName(u"current_autostart")
+        sizePolicy.setHeightForWidth(self.current_autostart.sizePolicy().hasHeightForWidth())
+        self.current_autostart.setSizePolicy(sizePolicy)
+
+        self.verticalLayout_4.addWidget(self.current_autostart)
+
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+
+        self.verticalLayout_3.addWidget(self.scrollArea)
+
+        self.add_program_button = QPushButton(self.auto_tab)
+        self.add_program_button.setObjectName(u"add_program_button")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.add_program_button.sizePolicy().hasHeightForWidth())
+        self.add_program_button.setSizePolicy(sizePolicy1)
+
+        self.verticalLayout_3.addWidget(self.add_program_button)
+
+        self.add_script_button = QPushButton(self.auto_tab)
+        self.add_script_button.setObjectName(u"add_script_button")
+        sizePolicy1.setHeightForWidth(self.add_script_button.sizePolicy().hasHeightForWidth())
+        self.add_script_button.setSizePolicy(sizePolicy1)
+
+        self.verticalLayout_3.addWidget(self.add_script_button)
+
+        self.del_autostart_button = QPushButton(self.auto_tab)
+        self.del_autostart_button.setObjectName(u"del_autostart_button")
+        sizePolicy1.setHeightForWidth(self.del_autostart_button.sizePolicy().hasHeightForWidth())
+        self.del_autostart_button.setSizePolicy(sizePolicy1)
+
+        self.verticalLayout_3.addWidget(self.del_autostart_button)
+
+
+        self.horizontalLayout_7.addLayout(self.verticalLayout_3)
+
+
+        self.verticalLayout_5.addLayout(self.horizontalLayout_7)
+
+        self.Menu_settings.addTab(self.auto_tab, "")
+        self.env_tab = QWidget()
+        self.env_tab.setObjectName(u"env_tab")
+        self.horizontalLayout_5 = QHBoxLayout(self.env_tab)
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.Menu_settings.addTab(self.env_tab, "")
+        self.look_tab = QWidget()
+        self.look_tab.setObjectName(u"look_tab")
+        self.horizontalLayout_6 = QHBoxLayout(self.look_tab)
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.Menu_settings.addTab(self.look_tab, "")
+        self.input_tab = QWidget()
+        self.input_tab.setObjectName(u"input_tab")
+        self.horizontalLayout_8 = QHBoxLayout(self.input_tab)
+        self.horizontalLayout_8.setObjectName(u"horizontalLayout_8")
+        self.Menu_settings.addTab(self.input_tab, "")
+        self.keybinds_tab = QWidget()
+        self.keybinds_tab.setObjectName(u"keybinds_tab")
+        self.Menu_settings.addTab(self.keybinds_tab, "")
+        self.windowrules_tab = QWidget()
+        self.windowrules_tab.setObjectName(u"windowrules_tab")
+        self.Menu_settings.addTab(self.windowrules_tab, "")
+
+        self.horizontalLayout_10.addWidget(self.Menu_settings)
 
 
         self.retranslateUi(Widget)
 
-        self.Menu_settings.setCurrentIndex(0)
+        self.Menu_settings.setCurrentIndex(1)
 
 
         QMetaObject.connectSlotsByName(Widget)
@@ -139,12 +213,16 @@ class Ui_Widget(object):
         self.label_2.setText(QCoreApplication.translate("Widget", u"Resolution", None))
         self.label_3.setText(QCoreApplication.translate("Widget", u"Position", None))
         self.label_4.setText(QCoreApplication.translate("Widget", u"Scale", None))
-        self.Menu_settings.setTabText(self.Menu_settings.indexOf(self.tab), QCoreApplication.translate("Widget", u"Monitor", None))
-        self.Menu_settings.setTabText(self.Menu_settings.indexOf(self.tab_2), QCoreApplication.translate("Widget", u"Environment", None))
-        self.Menu_settings.setTabText(self.Menu_settings.indexOf(self.tab_3), QCoreApplication.translate("Widget", u"Autostart", None))
-        self.Menu_settings.setTabText(self.Menu_settings.indexOf(self.tab_4), QCoreApplication.translate("Widget", u"Look and Feel", None))
-        self.Menu_settings.setTabText(self.Menu_settings.indexOf(self.tab_5), QCoreApplication.translate("Widget", u"Input", None))
-        self.Menu_settings.setTabText(self.Menu_settings.indexOf(self.tab_6), QCoreApplication.translate("Widget", u"Keybindings", None))
-        self.Menu_settings.setTabText(self.Menu_settings.indexOf(self.tab_7), QCoreApplication.translate("Widget", u"Window Rules", None))
+        self.apply_button.setText(QCoreApplication.translate("Widget", u"Apply", None))
+        self.Menu_settings.setTabText(self.Menu_settings.indexOf(self.monitor_tab), QCoreApplication.translate("Widget", u"Monitor", None))
+        self.add_program_button.setText(QCoreApplication.translate("Widget", u"Add Program", None))
+        self.add_script_button.setText(QCoreApplication.translate("Widget", u"Add Script", None))
+        self.del_autostart_button.setText(QCoreApplication.translate("Widget", u"Delete Autostart", None))
+        self.Menu_settings.setTabText(self.Menu_settings.indexOf(self.auto_tab), QCoreApplication.translate("Widget", u"Autostart", None))
+        self.Menu_settings.setTabText(self.Menu_settings.indexOf(self.env_tab), QCoreApplication.translate("Widget", u"Environment", None))
+        self.Menu_settings.setTabText(self.Menu_settings.indexOf(self.look_tab), QCoreApplication.translate("Widget", u"Look and Feel", None))
+        self.Menu_settings.setTabText(self.Menu_settings.indexOf(self.input_tab), QCoreApplication.translate("Widget", u"Input", None))
+        self.Menu_settings.setTabText(self.Menu_settings.indexOf(self.keybinds_tab), QCoreApplication.translate("Widget", u"Keybindings", None))
+        self.Menu_settings.setTabText(self.Menu_settings.indexOf(self.windowrules_tab), QCoreApplication.translate("Widget", u"Window Rules", None))
     # retranslateUi
 
--- widget.py
+++ widget.py
@@ -1,4 +1,5 @@
 import json
+import re
 import subprocess
 
 from PySide6.QtWidgets import QWidget
@@ -13,10 +14,20 @@ class Widget(QWidget, Ui_Widget):
         self.setWindowTitle("Hyprland Settings")
 
         # Monitor Settings
+        # TODO For Multi Monitor Setup
         self.monitors_box.addItems(self.get_monitor_names())
         self.resolution_box.addItems(self.get_monitor_resolution())
-        self.position_box.addItems(self.get_monitor_count())
+        # self.position_box.addItems(self.get_monitor_count())
+        # Needs to be fixed
+        self.position_box.addItem("auto")
+        self.scale_box.addItems(["1.0", "2.0"])
+        self.apply_button.clicked.connect(self.apply_monitor_settings)
 
+        # Environment Settings
+        self.current_autostart.addItems(self.get_current_autostarts())
+        self.del_autostart_button.clicked.connect(self.del_autostart)
+
+    # Monitor Funcions
     def get_monitor_names(self) -> list[str]:
         try:
             result = subprocess.run(
@@ -64,3 +75,58 @@ class Widget(QWidget, Ui_Widget):
         except (subprocess.CalledProcessError, json.JSONDecodeError) as e:
             print(f"An error occurred: {e}")
             return []
+
+    def apply_monitor_settings(self):
+        mon_name = self.monitors_box.currentText()
+        mon_res = self.resolution_box.currentText()
+        mon_pos = self.position_box.currentText()
+        mon_scale = self.scale_box.currentText()
+
+        config_file = "/home/est/Work/hypr-set/hyprland.conf"
+
+        new_line = f"monitor = {mon_name},{mon_res},{mon_pos},{mon_scale}"
+
+        pattern = r"^monitor\s*=.*"
+
+        with open(config_file, "r") as file:
+            content = file.read()
+
+        new_content = re.sub(pattern, new_line, content, flags=re.MULTILINE)
+
+        with open(config_file, "w") as file:
+            file.write(new_content)
+
+    # Autostart Functions
+    def get_current_autostarts(self) -> list[str]:
+        all_autostart = []
+
+        config_file = "/home/est/Work/hypr-set/hyprland.conf"
+
+        try:
+            with open(config_file, "r") as file:
+                for line in file:
+                    line = line.strip()
+                    if line.startswith("exec-once") and not line.startswith("#"):
+                        command = line.split("=", 1)[-1].strip()
+                        all_autostart.append(command)
+            return all_autostart
+        except FileNotFoundError:
+            print(f"Error: {config_file} not found.")
+            return all_autostart
+
+    def del_autostart(self):
+        config_file = "/home/est/Work/hypr-set/hyprland.conf"
+
+        current_row = self.current_autostart.currentRow()
+        if current_row != -1:
+            item = self.current_autostart.takeItem(current_row)
+            target = f"exec-once = {item.text()}"
+            with open(config_file, "r") as f:
+                lines = f.readlines()
+
+            with open(config_file, "w") as f:
+                for line in lines:
+                    if not line.strip().startswith(target):
+                        f.write(line)
+            print(target)
+            del item
