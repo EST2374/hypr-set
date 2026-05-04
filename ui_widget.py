b89fab79429df85bb24dda8a283e0386892ea44a
EST2374 <jack.cez2004@gmail.com>
2026-05-04 23:09:26 +0200

    autostart finished

--- ui_widget.py
+++ ui_widget.py
@@ -11,25 +11,37 @@
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
     QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
 from PySide6.QtWidgets import (QApplication, QComboBox, QHBoxLayout, QLabel,
-    QListWidget, QListWidgetItem, QPushButton, QScrollArea,
-    QSizePolicy, QTabWidget, QVBoxLayout, QWidget)
+    QListWidget, QListWidgetItem, QMainWindow, QMenu,
+    QMenuBar, QPushButton, QSizePolicy, QStatusBar,
+    QTabWidget, QVBoxLayout, QWidget)
 
 class Ui_Widget(object):
     def setupUi(self, Widget):
         if not Widget.objectName():
             Widget.setObjectName(u"Widget")
         Widget.resize(890, 716)
-        self.horizontalLayout_10 = QHBoxLayout(Widget)
+        self.actionThemes = QAction(Widget)
+        self.actionThemes.setObjectName(u"actionThemes")
+        self.dark_theme_button = QAction(Widget)
+        self.dark_theme_button.setObjectName(u"dark_theme_button")
+        self.light_theme_button = QAction(Widget)
+        self.light_theme_button.setObjectName(u"light_theme_button")
+        self.quit_program = QAction(Widget)
+        self.quit_program.setObjectName(u"quit_program")
+        self.centralwidget = QWidget(Widget)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.horizontalLayout_10 = QHBoxLayout(self.centralwidget)
         self.horizontalLayout_10.setObjectName(u"horizontalLayout_10")
-        self.Menu_settings = QTabWidget(Widget)
+        self.Menu_settings = QTabWidget(self.centralwidget)
         self.Menu_settings.setObjectName(u"Menu_settings")
         self.Menu_settings.setEnabled(True)
-        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Maximum)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.Menu_settings.sizePolicy().hasHeightForWidth())
@@ -123,47 +135,23 @@ class Ui_Widget(object):
         self.horizontalLayout_7.setObjectName(u"horizontalLayout_7")
         self.verticalLayout_3 = QVBoxLayout()
         self.verticalLayout_3.setObjectName(u"verticalLayout_3")
-        self.scrollArea = QScrollArea(self.auto_tab)
-        self.scrollArea.setObjectName(u"scrollArea")
-        self.scrollArea.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
-        self.scrollArea.setWidgetResizable(True)
-        self.scrollAreaWidgetContents = QWidget()
-        self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 709, 435))
-        self.verticalLayout_4 = QVBoxLayout(self.scrollAreaWidgetContents)
-        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
-        self.current_autostart = QListWidget(self.scrollAreaWidgetContents)
+        self.current_autostart = QListWidget(self.auto_tab)
         self.current_autostart.setObjectName(u"current_autostart")
-        sizePolicy.setHeightForWidth(self.current_autostart.sizePolicy().hasHeightForWidth())
-        self.current_autostart.setSizePolicy(sizePolicy)
 
-        self.verticalLayout_4.addWidget(self.current_autostart)
-
-        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
-
-        self.verticalLayout_3.addWidget(self.scrollArea)
+        self.verticalLayout_3.addWidget(self.current_autostart)
 
         self.add_program_button = QPushButton(self.auto_tab)
         self.add_program_button.setObjectName(u"add_program_button")
-        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Fixed)
-        sizePolicy1.setHorizontalStretch(0)
-        sizePolicy1.setVerticalStretch(0)
-        sizePolicy1.setHeightForWidth(self.add_program_button.sizePolicy().hasHeightForWidth())
-        self.add_program_button.setSizePolicy(sizePolicy1)
 
         self.verticalLayout_3.addWidget(self.add_program_button)
 
         self.add_script_button = QPushButton(self.auto_tab)
         self.add_script_button.setObjectName(u"add_script_button")
-        sizePolicy1.setHeightForWidth(self.add_script_button.sizePolicy().hasHeightForWidth())
-        self.add_script_button.setSizePolicy(sizePolicy1)
 
         self.verticalLayout_3.addWidget(self.add_script_button)
 
         self.del_autostart_button = QPushButton(self.auto_tab)
         self.del_autostart_button.setObjectName(u"del_autostart_button")
-        sizePolicy1.setHeightForWidth(self.del_autostart_button.sizePolicy().hasHeightForWidth())
-        self.del_autostart_button.setSizePolicy(sizePolicy1)
 
         self.verticalLayout_3.addWidget(self.del_autostart_button)
 
@@ -176,18 +164,34 @@ class Ui_Widget(object):
         self.Menu_settings.addTab(self.auto_tab, "")
         self.env_tab = QWidget()
         self.env_tab.setObjectName(u"env_tab")
-        self.horizontalLayout_5 = QHBoxLayout(self.env_tab)
-        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.verticalLayout_7 = QVBoxLayout(self.env_tab)
+        self.verticalLayout_7.setObjectName(u"verticalLayout_7")
+        self.verticalLayout_4 = QVBoxLayout()
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.current_env = QListWidget(self.env_tab)
+        self.current_env.setObjectName(u"current_env")
+
+        self.verticalLayout_4.addWidget(self.current_env)
+
+        self.add_env_button = QPushButton(self.env_tab)
+        self.add_env_button.setObjectName(u"add_env_button")
+
+        self.verticalLayout_4.addWidget(self.add_env_button)
+
+        self.del_env_button = QPushButton(self.env_tab)
+        self.del_env_button.setObjectName(u"del_env_button")
+
+        self.verticalLayout_4.addWidget(self.del_env_button)
+
+
+        self.verticalLayout_7.addLayout(self.verticalLayout_4)
+
         self.Menu_settings.addTab(self.env_tab, "")
         self.look_tab = QWidget()
         self.look_tab.setObjectName(u"look_tab")
-        self.horizontalLayout_6 = QHBoxLayout(self.look_tab)
-        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
         self.Menu_settings.addTab(self.look_tab, "")
         self.input_tab = QWidget()
         self.input_tab.setObjectName(u"input_tab")
-        self.horizontalLayout_8 = QHBoxLayout(self.input_tab)
-        self.horizontalLayout_8.setObjectName(u"horizontalLayout_8")
         self.Menu_settings.addTab(self.input_tab, "")
         self.keybinds_tab = QWidget()
         self.keybinds_tab.setObjectName(u"keybinds_tab")
@@ -198,17 +202,41 @@ class Ui_Widget(object):
 
         self.horizontalLayout_10.addWidget(self.Menu_settings)
 
+        Widget.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(Widget)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 890, 19))
+        self.menuTheme = QMenu(self.menubar)
+        self.menuTheme.setObjectName(u"menuTheme")
+        self.menu_Application = QMenu(self.menubar)
+        self.menu_Application.setObjectName(u"menu_Application")
+        Widget.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(Widget)
+        self.statusbar.setObjectName(u"statusbar")
+        Widget.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menuTheme.menuAction())
+        self.menubar.addAction(self.menu_Application.menuAction())
+        self.menuTheme.addSeparator()
+        self.menuTheme.addSeparator()
+        self.menuTheme.addAction(self.dark_theme_button)
+        self.menuTheme.addAction(self.light_theme_button)
+        self.menu_Application.addAction(self.quit_program)
 
         self.retranslateUi(Widget)
 
-        self.Menu_settings.setCurrentIndex(1)
+        self.Menu_settings.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(Widget)
     # setupUi
 
     def retranslateUi(self, Widget):
-        Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Form", None))
+        Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Hyprland Settings", None))
+        self.actionThemes.setText(QCoreApplication.translate("Widget", u"Themes", None))
+        self.dark_theme_button.setText(QCoreApplication.translate("Widget", u"Dark", None))
+        self.light_theme_button.setText(QCoreApplication.translate("Widget", u"Light", None))
+        self.quit_program.setText(QCoreApplication.translate("Widget", u"Quit", None))
         self.label.setText(QCoreApplication.translate("Widget", u"Monitor: ", None))
         self.label_2.setText(QCoreApplication.translate("Widget", u"Resolution", None))
         self.label_3.setText(QCoreApplication.translate("Widget", u"Position", None))
@@ -219,10 +247,14 @@ class Ui_Widget(object):
         self.add_script_button.setText(QCoreApplication.translate("Widget", u"Add Script", None))
         self.del_autostart_button.setText(QCoreApplication.translate("Widget", u"Delete Autostart", None))
         self.Menu_settings.setTabText(self.Menu_settings.indexOf(self.auto_tab), QCoreApplication.translate("Widget", u"Autostart", None))
+        self.add_env_button.setText(QCoreApplication.translate("Widget", u"Add Environment", None))
+        self.del_env_button.setText(QCoreApplication.translate("Widget", u"Delete Environment", None))
         self.Menu_settings.setTabText(self.Menu_settings.indexOf(self.env_tab), QCoreApplication.translate("Widget", u"Environment", None))
         self.Menu_settings.setTabText(self.Menu_settings.indexOf(self.look_tab), QCoreApplication.translate("Widget", u"Look and Feel", None))
         self.Menu_settings.setTabText(self.Menu_settings.indexOf(self.input_tab), QCoreApplication.translate("Widget", u"Input", None))
         self.Menu_settings.setTabText(self.Menu_settings.indexOf(self.keybinds_tab), QCoreApplication.translate("Widget", u"Keybindings", None))
         self.Menu_settings.setTabText(self.Menu_settings.indexOf(self.windowrules_tab), QCoreApplication.translate("Widget", u"Window Rules", None))
+        self.menuTheme.setTitle(QCoreApplication.translate("Widget", u"Themes", None))
+        self.menu_Application.setTitle(QCoreApplication.translate("Widget", u"Application", None))
     # retranslateUi
 
