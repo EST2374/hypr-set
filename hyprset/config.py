2b0a75c0529412c06a401a43b1a9c565940f416f
EST2374 <jack.cez2004@gmail.com>
2026-05-09 22:58:40 +0200

    quick fix

--- hyprset/config.py
+++ hyprset/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.parent
-CONFIG_FILE = BASE_DIR / "hyprland.conf"
-DEFAULT_CONFIG = BASE_DIR / "default_hyprland.conf"
+CONFIG_FILE = BASE_DIR / "examples/hyprland.conf"
+DEFAULT_CONFIG = BASE_DIR / "examples/default_hyprland.conf"
 REAL_CONFIG = Path.home() / ".config/hypr/hyprland.conf"
